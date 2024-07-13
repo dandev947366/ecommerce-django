ed675b2bde9546fca25592e7f7e61cf79f2823f5
danofoundation <dan.dev947366@gmail.com>
2024-07-13 14:28:02 +0300

    Update url for media

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -81,6 +81,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/ 'static/media'
 STATICFILES_DIRS = [BASE_DIR/ 'static']
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
