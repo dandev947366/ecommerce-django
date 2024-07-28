483d5c5b4ff9dfbf79044a1a7b3b230d5448aeda
danofoundation <dan.dev947366@gmail.com>
2024-07-28 13:10:26 +0300

    Update Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,19 +3,19 @@ import { Navbar, Nav, Container, Row } from 'react-bootstrap'
 function Header() {
   return (
       <header>
-       <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
+        <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
       <Container>
         <Navbar.Brand href="/">Ecommerce Shop</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/cart"><i class="fa-solid fa-cart-shopping"></i>Cart</Nav.Link>
-            <Nav.Link href="/login"><i class="fa-solid fa-right-to-bracket"></i>Login</Nav.Link>
-           
+          <Nav className="me-auto">
+            <Nav.Link href="/cart"><i class="fa-solid fa-cart-shopping"></i><span className="p-2">Cart</span></Nav.Link>
+            <Nav.Link href="/login"><i class="fa-solid fa-user"></i><span className="p-2">Login</span></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
+
       </header>
     
   )
