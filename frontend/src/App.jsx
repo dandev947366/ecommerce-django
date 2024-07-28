4487991cc7167783a09a6a3ddaca11883be60042
danofoundation <dan.dev947366@gmail.com>
2024-07-28 14:13:02 +0300

    Add Product component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
 function App() {
 
   return (
@@ -10,7 +11,7 @@ function App() {
      <main className="py-3">
      <Container>
   
-      <h1>Welcome</h1>
+      <HomeScreen  />
      </Container>
      
      </main>
