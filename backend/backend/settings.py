fc6d36c2453f34f2883f2a8048fb96bb090dfd89
danofoundation <dan.dev947366@gmail.com>
2024-07-28 17:36:55 +0300

    fetch data

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "base",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -121,5 +123,5 @@ STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
+CORS_ALLOW_ALL_ORIGINS=True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
