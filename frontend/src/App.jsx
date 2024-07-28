132263388a7cf619cc8ae0ff7af14dd7d890d158
danofoundation <dan.dev947366@gmail.com>
2024-07-28 15:51:17 +0300

    Add home route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,21 +2,24 @@ import { useState } from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { Container } from 'react-bootstrap'
-import HomeScreen from './screens/HomeScreen'
+import HomeScreen from './screens/HomeScreen';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 function App() {
 
   return (
-    <>
-     <Header  />
-     <main className="py-3">
-     <Container>
-  
-      <HomeScreen  />
-     </Container>
-     
-     </main>
-     <Footer  />
-    </>
+    <Router>
+    <Header />
+    <main className="py-3">
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          {/* <Route path="/about" element={<AboutScreen />} /> */}
+          {/* <Route path="/contact" element={<ContactScreen />} /> */}
+        </Routes>
+      </Container>
+    </main>
+    <Footer />
+  </Router>
   )
 }
 
